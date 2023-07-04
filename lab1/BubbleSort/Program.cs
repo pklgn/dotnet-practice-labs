c519caeecd7a6a4180117321c9c215a74f63dd34
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-07-04 22:56:18 +0300

    [lab1] apply .editorconfig

--- lab1/BubbleSort/Program.cs
+++ lab1/BubbleSort/Program.cs
@@ -1,43 +1,46 @@
-﻿class Program
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+class Program
 {
-				static void Main()
-				{
+    static void Main()
+    {
         Console.WriteLine("Test case 1");
-								List<int> input = new() { 30, 1, 3, -4 };
-								BubbleSort(input);
-								input.ForEach(item => Console.WriteLine(item));
+        List<int> input = new() { 30, 1, 3, -4 };
+        BubbleSort(input);
+        input.ForEach(item => Console.WriteLine(item));
 
         Console.WriteLine("\nTest case 2");
         List<string> stringInput = new() { "Dell", "Apple", "IBM", "Oracle", "cisco", "Cisco Systems" };
         BubbleSort(stringInput);
         stringInput.ForEach(item => Console.WriteLine(item));
-				}
+    }
 
-				static void BubbleSort<T>(IList<T> list, IComparer<T> comparer)
-				{
-								bool isSorted = true;
+    static void BubbleSort<T>(IList<T> list, IComparer<T> comparer)
+    {
+        bool isSorted = true;
 
-								if (list.Count > 2)
-								{
-												isSorted = false;
-								}
+        if (list.Count > 2)
+        {
+            isSorted = false;
+        }
 
-								while (!isSorted)
-								{
-												isSorted = true;
-												for (int i = 0; i < list.Count - 1; i++)
-												{
-																if (comparer.Compare(list[i], list[i + 1]) > 0)
-																{
-																				(list[i], list[i + 1]) = (list[i + 1], list[i]);
-																				isSorted = false;
-																}
-												}
-								}
-				}
+        while (!isSorted)
+        {
+            isSorted = true;
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                if (comparer.Compare(list[i], list[i + 1]) > 0)
+                {
+                    (list[i], list[i + 1]) = (list[i + 1], list[i]);
+                    isSorted = false;
+                }
+            }
+        }
+    }
 
-				static void BubbleSort<T>(IList<T> list)
-				{
-								BubbleSort<T>(list, Comparer<T>.Default);
-				}
+    static void BubbleSort<T>(IList<T> list)
+    {
+        BubbleSort<T>(list, Comparer<T>.Default);
+    }
 }
--- lab1/PersonalInfoReader/Program.cs
+++ lab1/PersonalInfoReader/Program.cs
@@ -1,4 +1,7 @@
-﻿void RunPersonDialog(TextReader input, TextWriter output, out Person person)
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+void RunPersonDialog(TextReader input, TextWriter output, out Person person)
 {
     void ReadLine(out string data)
     {
