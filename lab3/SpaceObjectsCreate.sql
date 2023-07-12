fcb9af195ea08c9d94e6e2472a58df548c520e3c
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-07-12 22:34:45 +0300

    [lab3] add create, insert and select examples

---
+++ lab3/SpaceObjectsCreate.sql
@@ -0,0 +1,52 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'SpaceObjects')
+BEGIN
+    CREATE DATABASE SpaceObjects;
+END
+
+USE SpaceObjects;
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='planet' and xtype='U')
+	CREATE TABLE planet (
+		[id_planet] INT IDENTITY PRIMARY KEY NOT NULL,
+		[id_planet_type] INT,
+		[id_star] INT,
+		[name] NVARCHAR(32),
+		[diameter] FLOAT(10),
+		[mass] FLOAT(10),
+		[surface_temperature] FLOAT(10),
+		[date_discovery] DATETIME
+	);
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='planet_type' and xtype='U')
+	CREATE TABLE planet_type (
+		[id_planet_type] INT IDENTITY PRIMARY KEY NOT NULL,
+		[name] NVARCHAR(32),
+		[description] NVARCHAR(256)
+	);
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='star' and xtype='U')
+	CREATE TABLE star (
+		[id_star] INT IDENTITY PRIMARY KEY NOT NULL,
+		[id_star_type] INT,
+		[name] NVARCHAR(32),
+		[diameter] FLOAT(10),
+		[mass] FLOAT(10),
+		[magnitude] FLOAT(2),
+		[temperature] FLOAT(10),
+		[date_discovery] DATETIME
+	);	
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='star_type' and xtype='U')
+	CREATE TABLE star_type (
+		[id_star_type] INT IDENTITY PRIMARY KEY NOT NULL,
+		[name] NVARCHAR(32)
+	);
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='star_has_absolute_magnitude' and xtype='U')
+	CREATE TABLE star_has_absolute_magnitude (
+		[id_star_has_absolute_magnitude] INT IDENTITY PRIMARY KEY NOT NULL,
+		[id_star] VARCHAR(1),
+		[absolute_magnitude] FLOAT(2),
+		[date_start] DATETIME,
+		[date_end] DATETIME
+	);
---
+++ lab3/SpaceObjectsSelect.sql
@@ -0,0 +1,48 @@
+﻿SELECT *
+    FROM [dbo].[planet];
+
+-- выбираем те звезды, чья звездная величина больше -20
+SELECT *
+	FROM [dbo].[star]
+	WHERE [magnitude] > -20;
+
+-- выбираем те планеты, имя которых начинается с RS
+SELECT *
+	FROM [dbo].[planet]
+	WHERE [name] LIKE 'RS%';
+
+-- выбираем звезды с перечисленными идентификаторами
+SELECT *
+    FROM [dbo].[star]
+    WHERE [id_star] IN (1, 2);
+
+-- выбираем планеты с массой от 100 до 500 масс Солнца
+SELECT *
+    FROM [dbo].[planet]
+    WHERE [mass] BETWEEN 100 AND 500;
+
+-- выбираем планеты, открытые либо в 2000, либо в 2001 году
+SELECT *
+    FROM [dbo].[planet]
+    WHERE YEAR([date_discovery]) = '2000' OR YEAR([date_discovery]) = '2001';
+
+-- выбираем все типы звезд, кроме типа O
+SELECT *
+    FROM [dbo].[star_type]
+    WHERE NOT [name] = 'Тип O';
+
+-- выбираем звезды с отсутствующей датой открытия
+SELECT *
+    FROM [dbo].[star]
+    WHERE [date_discovery] IS NULL;
+
+-- выбираем планеты, открытые в промежутке между 1999 и 2005 годами
+SELECT *
+    FROM [dbo].[planet]
+    WHERE YEAR([date_discovery]) BETWEEN '1999' AND '2005'
+    ORDER BY [date_discovery];
+
+-- подсчитываем количество планет, открытых в каждый из упомянутых годов
+SELECT YEAR([date_discovery]), COUNT(*) AS [planets_number]
+    FROM [dbo].[planet]
+    GROUP BY YEAR([date_discovery]);
