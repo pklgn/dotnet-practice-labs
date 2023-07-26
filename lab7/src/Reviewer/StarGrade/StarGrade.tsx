3c9039664c5eefd348b2b6dff5b7ebfd7e3a683c
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-07-26 13:37:25 +0300

    [lab7] small refactor; add setGrade function

--- lab7/src/Reviewer/StarGrade/StarGrade.tsx
+++ lab7/src/Reviewer/StarGrade/StarGrade.tsx
@@ -1,37 +1,46 @@
+import { MouseEvent, useEffect, useState } from "react";
 import Star from "../Star/Star";
 import styles from "./StarGrade.module.css";
 
 type StarGradeProps = {
-    value: number;
-    maxValue: number;
+    currentGrade: number;
+    maxGrade: number;
+    setGrade: (grade: number) => void;
 };
 
 function StarGrade(props: StarGradeProps) {
-    const { value, maxValue } = props;
-    const filledStars = Array.from({ length: value }, () => (
+    const { currentGrade, maxGrade, setGrade } = props;
+
+    // TODO: add isGrading or smth like that to commit user input
+    const filledStars = Array.from({ length: currentGrade }, () => (
         <Star state="filled" />
     ));
-    const defaultStars = Array.from({ length: maxValue - value }, () => (
+    const defaultStars = Array.from({ length: maxGrade - currentGrade }, () => (
         <Star state="default" />
     ));
 
-    function handleMouseOver(event: React.MouseEvent) {
-        const currentElement = event.target;
-        event.stopPropagation();
-        console.log("Current element:", currentElement);
+    function handleMouseOver(event: React.SyntheticEvent<HTMLDivElement>) {
+        if (event.currentTarget.dataset && event.currentTarget.dataset.index) {
+            setGrade(Number(event.currentTarget.dataset.index) + 1);
+        }
     }
 
     return (
         <div className={styles.starGrades}>
             {filledStars.length !== 0 &&
                 filledStars.map((filledStar, index) => (
-                    <div key={index} onMouseOver={handleMouseOver}>
+                    <div
+                        data-index={JSON.stringify(index)}
+                        key={index}
+                        onMouseOver={handleMouseOver}
+                    >
                         {filledStar}
                     </div>
                 ))}
             {defaultStars.length !== 0 &&
                 defaultStars.map((defaultStar, index) => (
                     <div
+                        data-index={JSON.stringify(index + filledStars.length)}
                         key={index + filledStars.length}
                         onMouseOver={handleMouseOver}
                     >
