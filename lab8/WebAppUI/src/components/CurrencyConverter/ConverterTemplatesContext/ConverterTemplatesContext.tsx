651c8cca9d93a7a285ed3791b69afed2a1836735
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-07-30 19:59:20 +0300

    [lab8] add several contexts to the common context

--- lab8/WebAppUI/src/components/CurrencyConverter/ConverterTemplatesContext/ConverterTemplatesContext.tsx
+++ lab8/WebAppUI/src/components/CurrencyConverter/ConverterTemplatesContext/ConverterTemplatesContext.tsx
@@ -1,29 +1,39 @@
 import { createContext, useState } from "react";
-import CurrencyConverter from "../CurrencyConverter";
+import CurrencyConverter, { Currency } from "../CurrencyConverter";
 
 export type ConverterTemplate = {
     sourceCode: string;
     targetCode: string;
 };
 
-const DEFAULT_CONVERTER_CURRENT_TEMPLATE = 0;
+const DEFAULT_CONVERTER_CURRENT_TEMPLATE_INDEX = 0;
 const DEFAULT_CONVERTER_TEMPLATE: ConverterTemplate = { sourceCode: "AUD", targetCode: "JPY" };
 
 // TODO: fix any type
 export const ConverterTemplatesContext = createContext<any>(null);
-export const ConverterCurrentTemplateContext = createContext<any>(null);
+export const ConverterCurrentTemplateIndexContext = createContext<any>(null);
+export const ConverterCurrenciesContext = createContext<any>(null);
+export const ConverterExchangeContext = createContext<any>(null);
 
-function ConverterTemplatesContextFC() {
-    const [currentTemplate, setCurrentTemplate] = useState<number>(DEFAULT_CONVERTER_CURRENT_TEMPLATE);
+function CurrencyConverterContext() {
+    const [currentTemplateIndex, setCurrentTemplateIndex] = useState<number>(DEFAULT_CONVERTER_CURRENT_TEMPLATE_INDEX);
     const [templates, setTemplates] = useState<ConverterTemplate[]>([DEFAULT_CONVERTER_TEMPLATE]);
+    const [currencies, setCurrencies] = useState<Currency[]>([]);
+    const [exchange, setExchange] = useState<ConverterTemplate>(DEFAULT_CONVERTER_TEMPLATE);
 
     return (
-        <ConverterTemplatesContext.Provider value={{ templates, setTemplates }}>
-            <ConverterCurrentTemplateContext.Provider value={{ currentTemplate, setCurrentTemplate }}>
-                <CurrencyConverter />
-            </ConverterCurrentTemplateContext.Provider>
-        </ConverterTemplatesContext.Provider>
+        <ConverterCurrenciesContext.Provider value={{ currencies, setCurrencies }}>
+            <ConverterTemplatesContext.Provider value={{ templates, setTemplates }}>
+                <ConverterCurrentTemplateIndexContext.Provider
+                    value={{ currentTemplate: currentTemplateIndex, setCurrentTemplate: setCurrentTemplateIndex }}
+                >
+                    <ConverterExchangeContext.Provider value={{ exchange, setExchange }}>
+                        <CurrencyConverter />
+                    </ConverterExchangeContext.Provider>
+                </ConverterCurrentTemplateIndexContext.Provider>
+            </ConverterTemplatesContext.Provider>
+        </ConverterCurrenciesContext.Provider>
     );
 }
 
-export default ConverterTemplatesContextFC;
+export default CurrencyConverterContext;
