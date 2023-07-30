5483dc5828d3baccd04b2b74e4387bd6f30ef408
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-07-30 19:57:26 +0300

    [lab8] add value and onChange to currency select

--- lab8/WebAppUI/src/components/Common/Select/Select.tsx
+++ lab8/WebAppUI/src/components/Common/Select/Select.tsx
@@ -1,15 +1,23 @@
+import { ChangeEventHandler } from "react";
 import styles from "./Select.module.css";
 
 export type SelectProps = {
+    value: string;
     options: string[];
     title?: string;
     className?: string;
+    onChange?: ChangeEventHandler<HTMLSelectElement>;
 };
 function Select(props: SelectProps) {
-    const { options, title, className } = props;
+    const { value, options, title, className, onChange } = props;
     return (
-        <select className={`${styles.select} ${className ?? ""}`} title={title ?? ""}>
-            {options.length !== 0 && options.map((option, index) => <option key={index}>{option}</option>)}
+        <select className={`${styles.select} ${className ?? ""}`} title={title ?? ""} value={value} onChange={onChange}>
+            {options.length !== 0 &&
+                options.map((option, index) => (
+                    <option key={index} value={option}>
+                        {option}
+                    </option>
+                ))}
         </select>
     );
 }
