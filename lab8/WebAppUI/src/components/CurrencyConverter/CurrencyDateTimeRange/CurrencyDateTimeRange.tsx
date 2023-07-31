cdefed7e67e7eb726e97169c79a94cd01efb93fc
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-07-31 20:59:26 +0300

    [lab8] add CurrencyDateTimeRange component

---
+++ lab8/WebAppUI/src/components/CurrencyConverter/CurrencyDateTimeRange/CurrencyDateTimeRange.tsx
@@ -0,0 +1,37 @@
+import { useContext } from "react";
+import { ConverterExchangeContext, ConverterTemplate } from "../ConverterTemplatesContext/ConverterTemplatesContext";
+import styles from "./CurrencyDateTimeRange.module.css";
+
+type CurrencyDateTimeRangeProps = {
+    selectedOption: number;
+    // each option is datetime in ms to currentDate
+    options: number[];
+};
+
+function CurrencyDateTimeRange(props: CurrencyDateTimeRangeProps) {
+    const { selectedOption, options } = props;
+    const { exchange, setExchange } = useContext(ConverterExchangeContext);
+
+    return (
+        <div className={styles.wrapper}>
+            {options.map((option: number, index: number) => {
+                return (
+                    <div
+                        key={index}
+                        className={`${styles.dateTime} ${selectedOption == option ? styles.selected : ""}`}
+                        onClick={() =>
+                            setExchange((prevExchange: ConverterTemplate) => ({
+                                ...prevExchange,
+                                dateTimeRange: option,
+                            }))
+                        }
+                    >
+                        {Math.floor(option / 1000 / 60)} min(s)
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
+
+export default CurrencyDateTimeRange;
