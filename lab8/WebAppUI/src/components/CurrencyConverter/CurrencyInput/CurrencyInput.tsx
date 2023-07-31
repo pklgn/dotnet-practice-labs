163da1f03a398ffe7d6a0926e44ec45a1cafeadf
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-07-31 20:31:03 +0300

    [lab8] add some styles and classNames in props

--- lab8/WebAppUI/src/components/CurrencyConverter/CurrencyInput/CurrencyInput.tsx
+++ lab8/WebAppUI/src/components/CurrencyConverter/CurrencyInput/CurrencyInput.tsx
@@ -1,8 +1,8 @@
 import { ChangeEventHandler, useContext } from "react";
+import { Currency } from "../../../model/CurrencyConverter/Currency";
 import Input, { InputType } from "../../Common/Input/Input";
 import Select from "../../Common/Select/Select";
 import { ConverterCurrenciesContext } from "../ConverterTemplatesContext/ConverterTemplatesContext";
-import { Currency } from "../CurrencyConverter";
 import styles from "./CurrencyInput.module.css";
 
 // TODO: maybe replace with common InputProps
@@ -22,7 +22,7 @@ function CurrencyInput(props: CurrencyInputProps) {
     const { type, selectedCurrency, onChange, onSelectChange, value, className } = props;
     return (
         <div className={`${styles.inputWrapper} ${className ?? ""}`}>
-            <Input type={type} value={value} onChange={onChange} />
+            <Input type={type} value={value} onChange={onChange} className={styles.input} />
             <div className={styles.selectWrapper}>
                 <div className={styles.selectDivider}></div>
                 <Select
@@ -30,6 +30,7 @@ function CurrencyInput(props: CurrencyInputProps) {
                     title={"Test"}
                     value={selectedCurrency}
                     onChange={onSelectChange}
+                    className={styles.select}
                 />
             </div>
         </div>
