e744cc72533d9367bcffe6f67808e05e154461d2
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-07-31 20:28:23 +0300

    [lab8] add converter constants

---
+++ lab8/WebAppUI/src/model/CurrencyConverter/ConverterConstants.ts
@@ -0,0 +1,10 @@
+export const DEFAULT_PRICE_UPDATE_IN_MILLISECONDS = 10 * 1000;
+
+export const DEFAULT_DATE_TIME_RANGES_IN_MILLISECONDS = [
+    1 * 6 * 10 * 1000, // One minute
+    2 * 6 * 10 * 1000, // Two minutes
+    5 * 6 * 10 * 1000, // Five minutes
+    10 * 6 * 10 * 1000, // Ten minutes
+];
+
+export const DEFAULT_PRICE = 1;
