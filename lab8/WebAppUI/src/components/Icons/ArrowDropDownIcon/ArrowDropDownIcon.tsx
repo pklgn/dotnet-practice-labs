2f84dbe4b0efe64aab81d45d9e1b10685c7fb103
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-07-31 20:29:07 +0300

    [lab8] make dropdown icon rotatable and fillable

--- lab8/WebAppUI/src/components/Icons/ArrowDropDownIcon/ArrowDropDownIcon.tsx
+++ lab8/WebAppUI/src/components/Icons/ArrowDropDownIcon/ArrowDropDownIcon.tsx
@@ -1,12 +1,23 @@
+import styles from "./ArrowDropDownIcon.module.css";
+
 export type ArrowDropDownIconProps = {
-    height: number;
     width: number;
+    height: number;
+    rotate?: 0 | 180;
+    fillColor?: "#FFFFFF" | "#000000";
 };
 
 function ArrowDropDownIcon(props: ArrowDropDownIconProps) {
+    const { width, height, rotate, fillColor } = props;
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" height={props.height} viewBox="0 -960 960 960" width={props.width}>
-            <path d="M480-360 280-559h400L480-360Z" />
+        <svg
+            className={rotate == 180 ? styles.reflectX : ""}
+            xmlns="http://www.w3.org/2000/svg"
+            width={width}
+            height={height}
+            viewBox="0 -960 960 960"
+        >
+            <path fill={fillColor ?? "000000"} d="M480-360 280-559h400L480-360Z" />
         </svg>
     );
 }
