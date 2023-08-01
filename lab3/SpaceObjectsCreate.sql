0326a1539bdb5514bdf0afc8150a9d09d569de1a
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-08-01 14:47:35 +0300

    [lab3] add NOT NULL #15

--- lab3/SpaceObjectsCreate.sql
+++ lab3/SpaceObjectsCreate.sql
@@ -10,44 +10,44 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='planet' and xtype='U')
 		[id_planet] INT IDENTITY PRIMARY KEY NOT NULL,
 		[id_planet_type] INT,
 		[id_star] INT,
-		[name] NVARCHAR(32),
+		[name] NVARCHAR(32) NOT NULL,
 		[diameter] FLOAT(10),
 		[mass] FLOAT(10),
 		[surface_temperature] FLOAT(10),
-		[date_discovery] DATETIME
+		[date_discovery] DATETIME NOT NULL
 	);
 
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='planet_type' and xtype='U')
 	CREATE TABLE planet_type (
 		[id_planet_type] INT IDENTITY PRIMARY KEY NOT NULL,
-		[name] NVARCHAR(32),
-		[description] NVARCHAR(256)
+		[name] NVARCHAR(32) NOT NULL,
+		[description] NVARCHAR(256) NOT NULL
 	);
 
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='star' and xtype='U')
 	CREATE TABLE star (
 		[id_star] INT IDENTITY PRIMARY KEY NOT NULL,
 		[id_star_type] INT,
-		[name] NVARCHAR(32),
+		[name] NVARCHAR(32) NOT NULL,
 		[diameter] FLOAT(10),
 		[mass] FLOAT(10),
 		[magnitude] FLOAT(2),
 		[temperature] FLOAT(10),
-		[date_discovery] DATETIME
+		[date_discovery] DATETIME NOT NULL
 	);	
 
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='star_type' and xtype='U')
 	CREATE TABLE star_type (
 		[id_star_type] INT IDENTITY PRIMARY KEY NOT NULL,
-		[name] NVARCHAR(32)
+		[name] NVARCHAR(32) NOT NULL
 	);
 
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='star_has_absolute_magnitude' and xtype='U')
 	CREATE TABLE star_has_absolute_magnitude (
 		[id_star_has_absolute_magnitude] INT IDENTITY PRIMARY KEY NOT NULL,
 		[id_star] INT,
-		[absolute_magnitude] FLOAT(2),
-		[date_start] DATETIME,
+		[absolute_magnitude] FLOAT(2) NOT NULL,
+		[date_start] DATETIME NOT NULL,
 		[date_end] DATETIME
 	);
 
