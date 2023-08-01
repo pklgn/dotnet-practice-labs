ad802c36fd7ba914d868fb2ff14887a71beb3b65
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-08-01 14:41:46 +0300

    [lab3] add FOREIGN KEYs #14

--- lab3/SpaceObjectsCreate.sql
+++ lab3/SpaceObjectsCreate.sql
@@ -45,8 +45,51 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='star_type' and xtype='U')
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='star_has_absolute_magnitude' and xtype='U')
 	CREATE TABLE star_has_absolute_magnitude (
 		[id_star_has_absolute_magnitude] INT IDENTITY PRIMARY KEY NOT NULL,
-		[id_star] VARCHAR(1),
+		[id_star] INT,
 		[absolute_magnitude] FLOAT(2),
 		[date_start] DATETIME,
 		[date_end] DATETIME
 	);
+
+-- planet
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='FK_planet_planet_type_id')
+BEGIN
+    ALTER TABLE planet
+    ADD CONSTRAINT FK_planet_planet_type_id
+    FOREIGN KEY (id_planet_type)
+    REFERENCES planet_type(id_planet_type)
+    ON UPDATE CASCADE
+    ON DELETE SET NULL;
+END
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='FK_planet_star_id')
+BEGIN
+    ALTER TABLE planet
+    ADD CONSTRAINT FK_planet_star_id
+    FOREIGN KEY (id_star)
+    REFERENCES star(id_star)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE;
+END
+
+-- star
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='FK_star_star_type_id')
+BEGIN
+    ALTER TABLE star
+    ADD CONSTRAINT FK_star_star_type_id
+    FOREIGN KEY (id_star_type)
+    REFERENCES star_type(id_star_type)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE;
+END
+
+-- star_has_absolute_magnitude
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='FK_star_has_absolute_magnitude_star_id')
+BEGIN
+    ALTER TABLE star_has_absolute_magnitude
+    ADD CONSTRAINT FK_star_has_absolute_magnitude_star_id
+    FOREIGN KEY (id_star)
+    REFERENCES star(id_star)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE;
+END
