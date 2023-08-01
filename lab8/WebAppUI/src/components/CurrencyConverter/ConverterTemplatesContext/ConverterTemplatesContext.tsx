2a35968f496c2f944d373234dcfc34192d12d1ff
Pavel Ermakov <petechka_petrov_201@mail.ru>
2023-08-01 16:08:20 +0300

    [lab8] fix indents

--- lab8/WebAppUI/src/components/CurrencyConverter/ConverterTemplatesContext/ConverterTemplatesContext.tsx
+++ lab8/WebAppUI/src/components/CurrencyConverter/ConverterTemplatesContext/ConverterTemplatesContext.tsx
@@ -1,9 +1,5 @@
 import { createContext, useState } from "react";
-import {
-    ConverterTemplate,
-    DEFAULT_CONVERTER_CURRENT_TEMPLATE_INDEX,
-    DEFAULT_CONVERTER_TEMPLATE,
-} from "../../../model/CurrencyConverter/ConverterConstants";
+import { ConverterTemplate, DEFAULT_CONVERTER_TEMPLATE } from "../../../model/CurrencyConverter/ConverterConstants";
 import { Currency } from "../../../model/CurrencyConverter/Currency";
 import CurrencyConverter from "../CurrencyConverter";
 
@@ -20,9 +16,9 @@ function CurrencyConverterContext() {
     return (
         <ConverterCurrenciesContext.Provider value={{ currencies, setCurrencies }}>
             <ConverterTemplatesContext.Provider value={{ templates, setTemplates }}>
-                    <ConverterExchangeContext.Provider value={{ exchange, setExchange }}>
-                        <CurrencyConverter />
-                    </ConverterExchangeContext.Provider>
+                <ConverterExchangeContext.Provider value={{ exchange, setExchange }}>
+                    <CurrencyConverter />
+                </ConverterExchangeContext.Provider>
             </ConverterTemplatesContext.Provider>
         </ConverterCurrenciesContext.Provider>
     );
